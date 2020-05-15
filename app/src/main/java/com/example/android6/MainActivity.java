1f4b155fba8fc2296fcc3c1b0fa84b4ae46e9b8a
Alexalys <58750438+Alexalys@users.noreply.github.com>
2020-05-15 13:41:10 +0300

    commit

---
+++ app/src/main/java/com/example/android6/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.android6;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.widget.Toast;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        registerReceiver(connectionBroadcastReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+    }
+    private BroadcastReceiver connectionBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            boolean connection = false;
+            ConnectivityManager connectivityManager =  (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+            if(activeNetwork != null && activeNetwork.isConnectedOrConnecting()){
+                connection = activeNetwork.isConnectedOrConnecting();
+            }
+
+            if (connection==true)
+            {
+                Toast.makeText(context,"Connected", Toast.LENGTH_LONG).show();
+
+            } else {
+                Toast.makeText(context,"No connection", Toast.LENGTH_LONG).show();
+            }
+        }
+    };
+}
+
